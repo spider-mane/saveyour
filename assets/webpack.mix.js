b19f76a893ddc9d9d8292ac08cb0219ffe57d02c
Chris Williams <spider.mane.web@gmail.com>
2021-12-30 20:45:16 -0500

    added new development support features and updated scaffolding

--- assets/webpack.mix.js
+++ assets/webpack.mix.js
@@ -1,8 +1,36 @@
-const mix = require("laravel-mix");
+const mix = require('laravel-mix');
 
+/**
+ * Basic settings
+ */
 mix
-  .setPublicPath("dist")
-  .setResourceRoot("src")
-  // .js('js/index.js', 'js')
-  .ts("ts/index.ts", "js")
-  .sass("sass/styles.css", "css");
+  .setResourceRoot('src')
+  .setPublicPath('dist')
+  .sourceMaps(true, 'eval-source-map', 'source-map')
+  .version();
+
+/**
+ * Browsersync
+ */
+mix.browserSync({proxy: 'saveyour.test'});
+
+/**
+ * Javascript
+ */
+mix
+  .js('src/js/index.js', 'dist/js/saveyour.js')
+  // .autoload({jquery: ['$', 'window.jQuery']})
+  .extract();
+
+/**
+ * Sass
+ */
+// mix
+// .sass('src/scss/main.scss', 'dist/css/saveyour.css', {
+//   sassOptions: {
+//     outputStyle: 'expanded',
+//   },
+// })
+// .options({
+//   processCssUrls: false,
+// });
---
+++ tests/bootstrap.php
@@ -0,0 +1,32 @@
+<?php
+
+use Dotenv\Dotenv;
+use WebTheory\Exterminate\Exterminator;
+
+use function Env\env;
+
+$root = dirname(__DIR__);
+
+require_once "$root/vendor/autoload.php";
+
+Dotenv::createUnsafeImmutable($root)->load();
+
+Exterminator::debug([
+    'enable' => env('DEBUG_ENABLE') ?? true,
+    'display' => env('DEBUG_DISPLAY') ?? true,
+    'log' => "$root/logs/saveyour.log",
+    'system' => [
+        'host_os' => env('HOST_OS'),
+        'host_path' => env('HOST_PATH'),
+        'guest_path' => env('GUEST_PATH'),
+    ],
+    'error_logger' => [
+        'channel' => env('LOG_CHANNEL'),
+    ],
+    'error_handler' => true,
+    'var_dumper' => [
+        'root' => $root,
+        'theme' => env('VAR_DUMP_THEME'),
+        'server_host' => env('VAR_DUMP_SERVER_HOST'),
+    ],
+]);
