66ac5557bed711dca5508a459360b06e7a0673c4
Chris Williams <spider.mane.web@gmail.com>
2022-04-07 04:34:42 -0400

    Mockery support added to PHPUnit setup

--- tests/Support/TestCase.php
+++ tests/Support/TestCase.php
@@ -6,10 +6,15 @@ namespace Tests\Support;
 
 use Faker\Factory;
 use Faker\Generator;
+use Mockery;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use Mockery\MockInterface;
 use PHPUnit\Framework\TestCase as PHPUnitTestCase;
 
 class TestCase extends PHPUnitTestCase
 {
+    use MockeryPHPUnitIntegration;
+
     protected Generator $faker;
 
     protected function setUp(): void
@@ -17,6 +22,17 @@ class TestCase extends PHPUnitTestCase
         parent::setUp();
 
         $this->faker = $this->createFaker();
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+        Mockery::close();
+    }
+
+    protected function makeMockeryOf(string $class)
+    {
+        return Mockery::mock($class);
     }
 
     protected function createFaker(): Generator
